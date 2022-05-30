9babc481bac32830a2394d3e30cffe916db1b720
Anand Tripathi <shadarsh23@gmail.com>
2022-05-30 13:24:15 +0530

    added iamges in campgrounds model

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati consectetur dolores aspernatur, accusantium est velit atque facere, voluptas eaque et, repudiandae optio harum eius quasi dolore officiis. Soluta, voluptatum expedita?",
+            price
         })
         await camp.save();
     }
