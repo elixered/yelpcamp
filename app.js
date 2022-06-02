e57ec8d668e64287ce28b40ab791bf935068e234
Anand Tripathi <shadarsh23@gmail.com>
2022-06-02 16:46:10 +0530

    added server side validations using joi

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const ejsMate = require('ejs-mate');
+const joi = require('joi');
+const { campgroundSchema } = require('./schemas.js');
 const Campground = require('./models/campground');
 const app = express();
 const mongoose = require('mongoose');
@@ -20,11 +22,23 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp',
 
 const path = require('path');
 const { error } = require('console');
+const Joi = require('joi');
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(', ');
+        throw new ExpressError(msg, 400);
+    }
+    else {
+        next();
+    }
+}
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -34,7 +48,7 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -55,7 +69,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground });
 }))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${id}`);
