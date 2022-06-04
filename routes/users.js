9fc7d45e2efb7e354fdfa60089d401718475f38b
Anand Tripathi <shadarsh23@gmail.com>
2022-06-04 19:34:41 +0530

    added animated stars rating

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,14 @@ const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const users = require('../controllers/users');
 
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.get('/register', users.renderRegister);
-router.post('/register', catchAsync(users.register));
-router.get('/login', users.renderLogin);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+
 router.get('/logout', users.logout);
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
 module.exports = router;
